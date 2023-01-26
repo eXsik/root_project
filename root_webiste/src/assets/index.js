344195db1c4266f797d119bb0be540ea5a31f07f
eXsik <princeoflurkers@gmail.com>
2023-01-26 12:53:08 +0100

    working on navbar

---
+++ root_webiste/src/assets/index.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import logo_white from './logo_white.png';
+import logo_org from './logo_org.png';
+import close from './close.svg';
+import menu from './menu.svg';
+import quotes from './quotes.svg';
+
+export {
+	logo,
+	logo_white,
+	logo_org,
+	close,
+	menu,
+	quotes
+}
